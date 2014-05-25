9586ad8d3a67e67635e715591e02c9c8ffacd24f
Greg Bueno <greg@gregbueno.com>
2014-05-25 14:28:59 -0700

    Migrating to Bootstrap.

--- app/views/crux/index.blade.php
+++ app/views/crux/index.blade.php
@@ -1,41 +1,45 @@
 @extends('crux.layout')
 
 @section('content')
-<div id="column-1">
-	<section id="introduction">
-		<p>Crux just thought it was another night of hunting – of seeking out those who would prey on humankind. Instead, the novice hunter ends up killing Thomas, one of the most powerful immortals in New York City, and starts a turf war that puts a bullseye on his back.</p>
+<div id="column-1" class="col-md-8">
+	<div class="row">
+		<section id="introduction" class="col-md-offset-1 col-md-11">
+			<p>Crux just thought it was another night of hunting – of seeking out those who would prey on humankind. Instead, the novice hunter ends up killing Thomas, one of the most powerful immortals in New York City, and starts a turf war that puts a bullseye on his back.</p>
 
-		<p>Too bad for his would-be captors. Thomas' death awakens in Crux a dormant power so strong, nobody – not even Crux himself – can fathom it.</p>
+			<p>Too bad for his would-be captors. Thomas' death awakens in Crux a dormant power so strong, nobody – not even Crux himself – can fathom it.</p>
 
-		<p>And he's just getting started ...</p>
-	</section>
+			<p>And he's just getting started ...</p>
+		</section>
+	</div>
+	
+	<div class="row">
+		<section id="about" class="col-md-6">
+			<h3>About the novel</h3>
 
-	<section id="about">
-		<h3>About the novel</h3>
+			<p>Crux has been a character that had been hanging around the edges of my consciousness since 1991. I've tried numerous times to set down his story, but I couldn't get his voice right.</p>
 
-		<p>Crux has been a character that had been hanging around the edges of my consciousness since 1991. I've tried numerous times to set down his story, but I couldn't get his voice right.</p>
+			<p>On my last attempt, I let two friends read the results, and they were eager to know what would happen to him. So I hunkered down and finished the story in 2009.</p>
 
-		<p>On my last attempt, I let two friends read the results, and they were eager to know what would happen to him. So I hunkered down and finished the story in 2009.</p>
+			<p><em>Starting to Remember</em> is the result.</p>
 
-		<p><em>Starting to Remember</em> is the result.</p>
+			<p>And it only took 18 years.</p>
+		</section>
 
-		<p>And it only took 18 years.</p>
-	</section>
+		<section id="why-free" class="col-md-6">
+			<h3>Why free?</h3>
 
-	<section id="why-free">
-		<h3>Why free?</h3>
+			<p>Crux isn't done with his adventures, but I don't consider myself as a full-time novelist. I did finish a literary novel back in 2001, which will remain unpublished.</p>
 
-		<p>Crux isn't done with his adventures, but I don't consider myself as a full-time novelist. I did finish a literary novel back in 2001, which will remain unpublished.</p>
+			<p>It took 8 years before I got around to finishing this story, 18 if you count the first drafts from 1991.</p>
 
-		<p>It took 8 years before I got around to finishing this story, 18 if you count the first drafts from 1991.</p>
+			<p>In short, don't make any bets on when the next Crux story will surface.</p>
 
-		<p>In short, don't make any bets on when the next Crux story will surface.</p>
-
-		<p>But I'd rather have some readers than no readers, and maybe if enough people like this tale, they'll put on the heat to make me finish another.</p>
-	</section>
+			<p>But I'd rather have some readers than no readers, and maybe if enough people like this tale, they'll put on the heat to make me finish another.</p>
+		</section>
+	</div>
 </div>
 
-<aside id="column-2">
+<aside id="column-2" class="col-md-4">
 	<h3>Read it now</h3>
 
 	<section id="download">
@@ -47,11 +51,15 @@
 			<li><a href="/files/crux/starting_to_remember.pdf" class="button">PDF</a></li>
 		</ul>
 
-		<a href="http://www.lulu.com/shop/greg-bueno/starting-to-remember/paperback/product-6160089.html"><img src="http://static.lulu.com/images/services/buy_now_buttons/us/gray.gif?20120904124441" border="0" alt="[Buy on Lulu]" title="[Buy on Lulu]" id="buy-icon" /></a>
+		<div class="row">
+			<div class="col-md-6">
+				Prefer print? <a href="http://www.lulu.com/shop/greg-bueno/starting-to-remember/paperback/product-6160089.html">Buy a paperback from Lulu</a>.
+			</div>
+			<div class="col-md-6">
+				<a href="http://www.lulu.com/shop/greg-bueno/starting-to-remember/paperback/product-6160089.html"><img src="http://static.lulu.com/images/services/buy_now_buttons/us/gray.gif?20120904124441" border="0" alt="[Buy on Lulu]" title="[Buy on Lulu]" id="buy-icon" /></a>
+			</div>
+		</div>
 
-		<p>
-			Prefer print? <a href="http://www.lulu.com/shop/greg-bueno/starting-to-remember/paperback/product-6160089.html">Buy a paperback from Lulu</a>.
-		</p>
 	</section>
 
 	<section id="cover">
